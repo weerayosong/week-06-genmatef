ee786e1d63290bfabc0f43ec84a6daf90f9f8f02
toonajuig <thirawat.devtt@gmail.com>
2026-04-08 15:12:44 +0700

    edit-conflict

---
+++ school/Teacher.js
@@ -0,0 +1,27 @@
+class Teacher {
+  constructor(teacherId, name, subject) {
+    this.teacherId = teacherId;
+    this.name = name;
+    this.subject = subject;
+  }
+
+  teachCourse(courseName) {
+    console.log(this.name + " is teaching " + courseName);
+  }
+
+  assignGrade(studentName, score) {
+    console.log(this.name + " gives " + score + " to " + studentName);
+  }
+
+  showProfile() {
+    console.log("Teacher ID: " + this.teacherId);
+    console.log("Name: " + this.name);
+    console.log("Subject: " + this.subject);
+  }
+}
+
+const t1 = new Teacher(1, "Mr. John", "Math");
+
+t1.showProfile();
+t1.teachCourse("Algebra");
+t1.assignGrade("Ali", 95);
--- school/teacher.js
+++ school/teacher.js
@@ -1,23 +1,23 @@
 class Teacher {
-    constructor(teacherId, name, subject) {
-        this.teacherId = teacherId;
-        this.name = name;
-        this.subject = subject;
-    }
+  constructor(teacherId, name, subject) {
+    this.teacherId = teacherId;
+    this.name = name;
+    this.subject = subject;
+  }
 
-    teachCourse(courseName) {
-        console.log(this.name + " is teaching " + courseName);
-    }
+  teachCourse(courseName) {
+    console.log(this.name + " is teaching " + courseName);
+  }
 
-    assignGrade(studentName, score) {
-        console.log(this.name + " gives " + score + " to " + studentName);
-    }
+  assignGrade(studentName, score) {
+    console.log(this.name + " gives " + score + " to " + studentName);
+  }
 
-    showProfile() {
-        console.log("Teacher ID: " + this.teacherId);
-        console.log("Name: " + this.name);
-        console.log("Subject: " + this.subject);
-    }
+  showProfile() {
+    console.log("Teacher ID: " + this.teacherId);
+    console.log("Name: " + this.name);
+    console.log("Subject: " + this.subject);
+  }
 }
 
 const t1 = new Teacher(1, "Mr. John", "Math");
